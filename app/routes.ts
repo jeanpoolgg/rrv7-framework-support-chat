f93ca3c6f0b9c5842e6af941ec8b4fddcb3f81e0
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-04 18:08:57 -0500

    feat(routes): set up initial route config with auth and home paths

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,20 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+	index,
+	layout,
+	prefix,
+	type RouteConfig,
+	route,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+	// Home
+	index("routes/home.tsx"),
+
+	// Auth: /auth/login,  /auth/register
+	...prefix("/auth", [
+		layout("layouts/auth-layout.tsx", [
+			route("login", "routes/auth/login-page.tsx"),
+			route("register", "routes/auth/register-page.tsx"),
+		]),
+	]),
+] satisfies RouteConfig;
