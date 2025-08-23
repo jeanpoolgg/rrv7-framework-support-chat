058ee45b65f9bb26c1b88025e5507b050e0b31d6
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-23 05:05:41 -0500

    feat(route-module): add client clientLoader function

--- app/routes/auth/testing-page.tsx
+++ app/routes/auth/testing-page.tsx
@@ -1,12 +1,42 @@
 import { Link } from "react-router";
+import type { Route } from "./+types/testing-page";
 
-const TestingPage = () => {
+export async function loader() {
+	console.log("Hola Mundo desde el loader - Server");
+	return { message: "Hola Mundo desde el loader - Server" };
+}
+
+export async function clientLoader({ serverLoader }: Route.ClientLoaderArgs) {
+	console.log("Hola Mundo desde el clientLoader - Client");
+
+	// call the server loader
+	const serverData = await serverLoader();
+
+	return {
+		message: "Hola Mundo desde el clientLoader - Client",
+		serverData: serverData,
+	};
+}
+
+export default function TestingPage({
+	loaderData,
+	actionData,
+	params,
+	matches,
+}: Route.ComponentProps) {
 	return (
 		<div>
-			<h1>Testing Page</h1>
-			<Link to="/auth/login">Login</Link>
+			<h1 className="font-bold text-2xk">Testing Page</h1>
+			<p>Loader Data: {JSON.stringify(loaderData)}</p>
+			<p>Action Data: {JSON.stringify(actionData)}</p>
+			<p>Route Parameters: {JSON.stringify(params)}</p>
+			<p>Matched Routes: {JSON.stringify(matches)}</p>
+			<Link
+				to="/auth/testing-args"
+				className="text-blue-500 underline text-2xl"
+			>
+				Testing Args
+			</Link>
 		</div>
 	);
-};
-
-export default TestingPage;
+}
