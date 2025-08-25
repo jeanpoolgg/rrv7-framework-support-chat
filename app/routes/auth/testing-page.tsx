5e9d15f7393c08e902390ba39ea8f8eafff31dee
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-25 10:25:58 -0500

    feat(actions): add action and clientAction handlers

--- app/routes/auth/testing-page.tsx
+++ app/routes/auth/testing-page.tsx
@@ -1,5 +1,36 @@
-import { NavLink } from "react-router";
+import { Form, NavLink } from "react-router";
+import { sleep } from "~/lib/sleep";
 import type { Route } from "./+types/testing-page";
+
+export async function action({ request }: Route.ActionArgs) {
+	await sleep(1000);
+
+	const data = await request.formData();
+	const name = data.get("name");
+	const allData = Object.fromEntries(data);
+
+	console.log("Server Side - Actions");
+	console.log({ name, allData });
+
+	return { ok: true, message: "Todo bien" };
+}
+
+export async function clientAction({
+	serverAction,
+	request,
+}: Route.ClientActionArgs) {
+	await sleep(1000);
+	const formData = await request.clone().formData();
+	const allData = Object.fromEntries(formData);
+
+	const data = await serverAction();
+
+	return {
+		message: "Hola Mundo desde el clientAction - Client",
+		data,
+		allData,
+	};
+}
 
 export async function loader() {
 	console.log("Hola Mundo desde el loader - Server");
@@ -39,6 +70,25 @@ export default function TestingPage({
 			>
 				Testing Args
 			</NavLink>
+
+			<Form className="mt-2 flex gap-2" method="post">
+				<input
+					className="border-2 border-gray-300 rounded-md p-2"
+					type="text"
+					name="name"
+				/>
+				<input
+					className="border-2 border-gray-300 rounded-md p-2"
+					type="text"
+					name="age"
+				/>
+				<button
+					type="submit"
+					className="cursor-pointer bg-blue-500 text-white rounded-md p-2"
+				>
+					Submit
+				</button>
+			</Form>
 		</div>
 	);
 }
