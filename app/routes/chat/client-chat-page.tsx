5d6c9f62abf70339c9c95686007a2b88b9c04ab2
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-26 21:42:35 -0500

    feat(client-chat): send message to client

--- app/routes/chat/client-chat-page.tsx
+++ app/routes/chat/client-chat-page.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { Button } from "~/components/ui/button";
 import { ScrollArea } from "~/components/ui/scroll-area";
 import { Textarea } from "~/components/ui/textarea";
-import { getClientMessages } from "~/fake/fake-data";
+import { getClientMessages, sendMessage } from "~/fake/fake-data";
 import { formatDate } from "~/lib/date-formatter";
 import type { Route } from "./+types/client-chat-page";
 
@@ -11,6 +11,18 @@ export async function loader({ params }: Route.LoaderArgs) {
 	const idClient = params.id;
 	const data = await getClientMessages(idClient);
 	return data;
+}
+
+export async function action({ params, request }: Route.ActionArgs) {
+	const formData = await request.formData();
+	const message = `${formData.get("message")}`;
+
+	const newMessage = await sendMessage({
+		sender: "agent",
+		clientId: params.id,
+		content: message,
+		createdAt: new Date(),
+	});
 }
 
 const ClientChatPage = ({ loaderData }: Route.ComponentProps) => {
@@ -34,16 +46,16 @@ const ClientChatPage = ({ loaderData }: Route.ComponentProps) => {
 									<div className="space-y-2">
 										<div className="flex items-center gap-2">
 											<span className="text-sm font-medium">NexTalk</span>
-											<span className="text-sm text-muted-foreground">
-												{formatDate(message.createdAt)}
-											</span>
 										</div>
-										<div className="p-3 bg-muted/50 rounded-lg">
+										<div className="p-3 mb-0 bg-muted/50 rounded-lg">
 											<p className="text-sm whitespace-pre-wrap">
 												{message.content}
 											</p>
 										</div>
-										<div className="flex items-center gap-2">
+										<span className="text-sm text-muted-foreground">
+											{formatDate(message.createdAt)}
+										</span>
+										<div className="flex items-center gap-2 mt-2">
 											<Button variant="ghost" size="icon" className="h-8 w-8">
 												<Copy className="h-4 w-4" />
 											</Button>
@@ -64,15 +76,15 @@ const ClientChatPage = ({ loaderData }: Route.ComponentProps) => {
 								<div className="flex flex-col items-end">
 									<div className="text-right mb-1">
 										<span className="text-sm font-medium mr-2">G5</span>
-										<span className="text-sm text-muted-foreground">
-											{formatDate(message.createdAt)}
-										</span>
 									</div>
 									<div className="bg-black text-white p-3 rounded-lg max-w-[80%]">
 										<p className="text-sm whitespace-pre-wrap">
 											{message.content}
 										</p>
 									</div>
+									<span className="text-sm text-muted-foreground">
+										{formatDate(message.createdAt)}
+									</span>
 								</div>
 							)}
 						</div>
@@ -81,18 +93,24 @@ const ClientChatPage = ({ loaderData }: Route.ComponentProps) => {
 			</ScrollArea>
 
 			<div className="p-4 border-t">
-				<div className="flex items-center gap-2">
-					<Textarea
-						placeholder="Type a message as a customer"
-						value={input}
-						onChange={(e) => setInput(e.target.value)}
-						className="min-h-[44px] h-[44px] resize-none py-3"
-					/>
-					<Button className="h-[44px] px-4 flex items-center gap-2">
-						<Send className="h-4 w-4" />
-						<span>Send</span>
-					</Button>
-				</div>
+				<form method="post">
+					<div className="flex items-center gap-2">
+						<Textarea
+							placeholder="Type a message as a customer"
+							value={input}
+							name="message"
+							onChange={(e) => setInput(e.target.value)}
+							className="min-h-[44px] h-[44px] resize-none py-3"
+						/>
+						<Button
+							type="submit"
+							className="h-[44px] px-4 flex items-center gap-2 cursor-pointer"
+						>
+							<Send className="h-4 w-4" />
+							<span>Send</span>
+						</Button>
+					</div>
+				</form>
 			</div>
 		</div>
 	);
