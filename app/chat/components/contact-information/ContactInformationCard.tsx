31126c225c0b60209a95e990ec1024536db62901
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-26 20:42:30 -0500

    feat(contact): integrate useLoaderData in ContactInformationCard

--- app/chat/components/contact-information/ContactInformationCard.tsx
+++ app/chat/components/contact-information/ContactInformationCard.tsx
@@ -1,10 +1,23 @@
-// import { ContactInformation } from "./ContactInformation";
-// import { ContactInformationSkeleton } from "./ContactInformationSkeleton";
+import { useLoaderData, useNavigation, useParams } from "react-router";
+import type { Client } from "~/chat/interfaces/chat.interface";
+import { ContactInformation } from "./ContactInformation";
 import { ContactInformationSkeleton } from "./ContactInformationSkeleton";
-// import { NoContactSeleted } from "./NoContactSeleted";
+import { NoContactSeleted } from "./NoContactSeleted";
 
 export const ContactInformationCard = () => {
-	// return <NoContactSeleted />;
-	return <ContactInformationSkeleton />;
-	// return <ContactInformation />;
+	const { id } = useParams();
+	const { clients = [] } = useLoaderData();
+	const { state } = useNavigation();
+
+	const isPending = state === "loading";
+
+	if (isPending) return <ContactInformationSkeleton />;
+
+	if (!id) return <NoContactSeleted />;
+
+	const client = clients.find((client: Client) => client.id === id);
+
+	if (!client) return <NoContactSeleted />;
+
+	return <ContactInformation client={client} />;
 };
